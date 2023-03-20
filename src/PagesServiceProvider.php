95e6dbfe679b9bdb54f34c2f07f6a298f2714375
Niko <nikolaistehl@werbewolke.de>
2023-03-20 16:33:50 +0100

    refactor

--- src/PagesServiceProvider.php
+++ src/PagesServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace Werbewolke\Pages;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class PagesServiceProvider extends ServiceProvider
 {
+    use AutomaticServiceProvider;
+    
     protected $vendorName = 'werbewolke';
     protected $packageName = 'pages';
     protected $commands = [
