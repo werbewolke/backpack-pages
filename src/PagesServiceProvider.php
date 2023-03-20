2473320a844a479eb569e9f51068ea76d1cff25b
Niko <nikolaistehl@werbewolke.de>
2023-03-20 16:59:54 +0100

    wip

--- src/PagesServiceProvider.php
+++ src/PagesServiceProvider.php
@@ -7,24 +7,11 @@ use Illuminate\Support\ServiceProvider;
 
 class PagesServiceProvider extends ServiceProvider
 {
-//    use AutomaticServiceProvider;
+    use AutomaticServiceProvider;
 
     protected $vendorName = 'werbewolke';
     protected $packageName = 'pages';
     protected $commands = [
         \Werbewolke\Pages\Console\Commands\Install::class,
     ];
-
-    protected function bootForConsole(): void
-    {
-
-        $this->publishes([
-            __DIR__.'/../publish' => './',
-        ]);
-
-        // Registering package commands.
-        if (! empty($this->commands)) {
-            $this->commands($this->commands);
-        }
-    }
 }
