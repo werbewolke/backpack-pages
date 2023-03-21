151ec716b3093d1eacad5effb414523701579e3f
Niko <nikolaistehl@werbewolke.de>
2023-03-21 11:54:02 +0100

    clean ServiceProvider

--- src/PagesServiceProvider.php
+++ src/PagesServiceProvider.php
@@ -7,11 +7,40 @@ use Illuminate\Support\ServiceProvider;
 
 class PagesServiceProvider extends ServiceProvider
 {
-    use AutomaticServiceProvider;
-
     protected $vendorName = 'werbewolke';
     protected $packageName = 'pages';
     protected $commands = [
         \Werbewolke\Pages\Console\Commands\Install::class,
     ];
+
+    /**
+     * Boot method may be overrided by AddonServiceProvider
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+
+        $this->publishes([
+            __DIR__.'/../publish' => './',
+        ]);
+
+        // Registering package commands.
+        if (! empty($this->commands)) {
+            $this->commands($this->commands);
+        }
+    }
 }
